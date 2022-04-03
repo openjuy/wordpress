ee637916e3502bf41f6286f5aada16e7a810a18e
Seba Madrid <openjuy@gmail.com>
2022-04-03 15:43:18 -0300

    Se agrego el volumen php.ini al contenedor php-openjuy

--- .docker/descargarWordpress.sh
+++ .docker/descargarWordpress.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-
-FILE=./html
-FILE2=./nginx-conf
-if [ -f "$FILE" ]; then
-    echo "El directorio html ya existe"
-else 
-    echo "El directorio html no existe"
-    mkdir html
-fi
-
-if [ -f "$FILE2" ]; then
-    echo "El directorio nginx-conf ya existe"
-else 
-    echo "El directorio nginx-conf no existe"
-    mkdir nginx-conf
-fi
-
 cd html
 echo 'Bienvenido al script de descarga de wordpress'
 echo 'Por favor ingrese el nombre del directorio del proyecto'
